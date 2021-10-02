6cd1bd7a3aba04f74ebd5e43d9588b92d71a1bdc
Mr. J <45538844+Ultraviolet-Ninja@users.noreply.github.com>
2021-10-02 13:37:05 -0500

    Initial Commit

---
+++ src/heap/Main.java
@@ -0,0 +1,26 @@
+package heap;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        PermutationGenerator<String> obj = new PermutationGenerator<>();
+        Character[] cArr = {'c', 'd', 'w', 'e', 'r', '4', '2', '1', '0', '6', '%'};
+        String[] splitter = "asdwfjtylom".split("");
+        Set<String[]> verification = new HashSet<>();
+        long start = System.nanoTime();
+        obj.heapPermutation(splitter, verification);
+        long stop = System.nanoTime();
+
+//        int i = 0;
+//        while (i < permutations.size()) {
+//            System.out.println(Arrays.toString(permutations.get(i)));
+//            i++;
+//        }
+        System.out.println(verification.size());
+        System.out.println(stop - start);
+    }
+}
